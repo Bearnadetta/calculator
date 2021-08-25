0503aeb28529922402aa8d2abc63bcab5c35aa2c
Bearnadetta <bearnadetta@gmail.com>
2021-08-25 09:58:39 -0700

    added css color palette, fixed button grid

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+
+//adds two numbers
 const add = function(a, b) {
     return (a + b);
 };
-
+//subtracts b from a
 const subtract = function(a, b) {
     return (a - b);
 }
-
+//multiplies two numbers
 const multiply = function(a, b) {
     return (a * b);
 }
-
+//divides a by b, returns remainder in rounded decimal
 const divide = function(a, b) {
+    //prevent divide by 0
     if(b == 0) {
         return 'ERROR';
     }else if ((a % b ) != 0) {
@@ -19,7 +22,7 @@ const divide = function(a, b) {
         return (a / b);
     }
 }
-
+//calls one of the operator functions with two numbers
 const operate = function(operator, num1, num2) {
     if(operator == 'plus') {
         return add(num1, num2);
